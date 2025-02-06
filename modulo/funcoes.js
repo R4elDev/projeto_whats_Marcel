304ca6fa7648500a554061cc46c803e289dca3a2
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-02-06 17:04:25 -0300

    Terminando a aplicacao

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -100,18 +100,18 @@ const getConversasDeCadaUsuario = function(telefone){
 
 const getConversaEContato = function(telefone,contato){
     let entrada = String(telefone)
-    let segundaEntrada = String(contato)
+    let segundaEntrada = String(contato).toLowerCase()
     let base = listaDeContatos.contatos.whats_users
     let status = false
     let conversas = {
         nome:entrada,
         contato:segundaEntrada,
-        mensagens:[],
+        mensagens:[]
     }
 
     base.forEach(function(item){
         item.contacts.forEach(function(itemContato){
-            if(String(item.number) === entrada && String(itemContato.name) === segundaEntrada){
+            if(String(item.number) == entrada && String(itemContato.name).toLowerCase() == segundaEntrada){
                 status = true
                 conversas.mensagens.push(itemContato.messages)
             }
@@ -125,5 +125,48 @@ const getConversaEContato = function(telefone,contato){
     }
 }
 
-console.log(getConversaEContato('11987876567','Julia Smith'))
+const pesquisaDePalavraChave = function(telefone,palavra,contato){
+    let tell = String(telefone)
+    let word = String(palavra).toLowerCase()
+    let cont = String(contato).toLowerCase()
+    let base = listaDeContatos.contatos.whats_users
+    let status = false
 
+    let filtragem ={
+        nome: tell,
+        contato: cont,
+        palavra_chave: word,
+        mensagens: []
+    }
+
+    base.forEach(function(item){
+        console.log('forEach 1')
+        item.contacts.forEach(function(itemContato){
+            if(String(item.number) == tell && String(itemContato.name).toLowerCase() == cont){
+                itemContato.messages.forEach(function(itemMensagem){
+                    if(itemMensagem.content.toLowerCase().includes(word)){
+                        status = true
+                        filtragem.mensagens.push(itemMensagem)
+                    }
+                })
+            }
+        })
+    })
+
+    if(status){
+        return filtragem
+    }else{
+        return false
+    }
+}
+
+console.log(getConversaEContato('11966578996', 'José Maria da Silva'))
+
+module.exports = {
+    getDadosPessoaisPorUsuario,
+    getDadosContaDoUsuario,
+    getDadosDeContatosParaCadaUsuario,
+    getConversasDeCadaUsuario,
+    getConversaEContato,
+    pesquisaDePalavraChave
+  }
